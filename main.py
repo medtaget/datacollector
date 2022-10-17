34bc169150ed739488c0d4cf9169418540e6ce54
medtaget <37709109+medtaget@users.noreply.github.com>
2022-10-17 18:35:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import requests
 import json
-from xbet import underSearcher
+#from pprint import pprint
 import time
 import schedule
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
-import cloudscraper
-from fake_useragent import UserAgent
+
 # Connect to Google Sheets
 scope = ['https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive"]
@@ -15,86 +14,122 @@ scope = ['https://www.googleapis.com/auth/spreadsheets',
 credentials = ServiceAccountCredentials.from_json_keyfile_name("gs_credentials.json", scope)
 client = gspread.authorize(credentials)
 
-def signals():
-  # Open the spreadsheet
-  db=[]
-  sheet = client.open("bettingscraper").sheet1
-  dbold=sheet.get_all_records()
-  print(dbold)
-  ua = UserAgent()
-  headers = {
-      'authority': 'games.scoretrend.net',
-      'accept': '*/*',
-      'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,ar;q=0.6',
-      'cache-control': 'no-cache',
-      'origin': 'https://scoretrend.net',
-      'pragma': 'no-cache',
-      'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
-      'sec-ch-ua-mobile': '?0',
-      'sec-ch-ua-platform': '"Windows"',
-      'sec-fetch-dest': 'empty',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-site': 'same-site',
-      'user-agent': str(ua.random),
-  }
-  #scraper = cloudscraper.create_scraper()
-  #r = scraper.get("MY API").text
-  response = requests.get('https://games.scoretrend.net/', headers=headers).text
-  print("#######")
-  print(response)
-  print("#######")
-  data= json.loads(response)
-  for d in data[0]:
-     #pprint(d)
-   try:
-     
-     #d["goalInterceptor"] <= 150
-     if (d['score']=='0:0' or d['score']=='1:0' or d['score']=='0:1' or d['score']=='2:0' or d['score']=='0:2'or d['score']=='2:1' or d['score']=='1:1' or d['score']=='1:2' ) and 50 <= d['timer']['tm'] <= 70:
+score=['0:0','0:1','1:0','1:1','1:2','2:1','2:0','0:2']
+
+def signals ():
+
+# Open the spreadsheet
+ db=[]
+ sheet = client.open("bettingscraper").sheet1
+ dbold=sheet.get_all_records()
+ print(dbold)
+  
+ params={
+        "sports": 1,
+        "country": 1,
+        "lng":'en',
+        "count": 10000,
+        "getEmpty":"true",
+        
       
-        response=requests.get('https://api.scoretrend.net/event/'+d['eventid'],headers=headers).text
-        event= json.loads(response)
-        #pprint(event)
-        print("########################")
-        print("League: "+event["data"][1])
-        print("Match: "+event["data"][2]+" VS "+event["data"][3])
-        print("Score: "+d["score"])
-        print("GT: "+str(d["goalInterceptor"]))
-        print("time: "+str(d['timer']['tm'])+"'")
-        try:
-         under,odd= underSearcher(event["data"][2]+" "+event["data"][3],d['score'])
-         print("under "+str(under)+" : "+str(odd))
-        
-         dict={
-          "League":event["data"][1],
-          "Home" :event["data"][2],
-           "Away":event["data"][3],
-          "Score":d["score"],
-          "GT":str(d["goalInterceptor"]),
-          "time":d['timer']['tm'],
-          "Under":under,
-          "Odd":odd
-        }
-         db.append(dict)
-        except:
-          print("odds not found")
-        
+    }
 
-         #print(link)
-         #notifSignal15(event["data"][1],event["data"][2],event["data"][3],d["score"],d["goalInterceptor"],d['timer']['tm'],over15,link) 
-               
-   except:
-      print("error")
-      pass
-   
-  df = pd.DataFrame()
-  dbold.extend(db)
-  df = df.append(dbold, ignore_index=True) 
-  dfi  = df.drop_duplicates(subset=["Home", "Away"], keep='first')
-  sheet.update([dfi.columns.values.tolist()] +dfi.values.tolist())
+ headers={
+        "Accept": "*/*",
+        "DNT": "1",
+        "Referer": "https://1xbet.com/fr/live/Football/",
+        "Sec-Fetch-Mode": "cors",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest"
 
 
-# scheduling steps
-schedule.every(30).seconds.do(signals)
+    }
+ response =   requests.get("https://1xbet.com/LiveFeed/Get1x2_Zip", headers=headers,params=params).text
+
+ data = json.loads(response)
+
+ #pprint((data["Value"]))
+
+ for dt in data["Value"]:
+  #pprint(dt) 
+  try:
+  
+    
+     params = {
+      'id': dt["ZP"],
+      'lng': 'en',
+      'cfview': '0',
+      'isSubGames': 'true',
+      'GroupEvents': 'true',
+      'allEventsGroupSubGames': 'true',
+      'countevents': '500',
+      'partner': '213',
+      'marketType': '1',
+      'isNewBuilder': 'true',
+   }
+
+     response = requests.get("https://1xbet.com/LiveFeed/GetGameZip", headers=headers,params=params).text
+
+     data = json.loads(response)
+     g=dt['SC']['FS']
+     if ('S1' in g.keys()):
+      s1=g['S1']
+     else:
+      s1='0'
+     if ('S2' in g.keys()):
+      s2=g['S2']
+     else:
+      s2='0'
+     s=str(s1)+':'+str(s2)
+     t=dt['SC']["TS"]
+     l=dt['LE']
+     match=dt["O1"]+' vs '+dt["O2"]
+  
+     if 3000<=t<=4200 and (s in score):
+      try:
+       
+       print(l)
+       print(match)
+       print(s)
+       print(round(t/60))
+       print("##############")
+
+  
+       for dt in data["Value"]["GE"]:
+        if dt['G']==99:
+         for under in dt['E'][1]:
+           sc=int(s.split(":")[0])+int(s.split(":")[1])+3.5
+           if 1.05<= under['C'] <= 1.2 and under['P']>sc:
+            print('trade found')
+            print(under['P'])
+            print(under['C'])
+            
+            dict={
+           "League":l,
+          "Home" :match.split(' vs ')[0],
+           "Away":match.split(' vs ')[1] ,
+          "Score":s,
+          "Time":round(t/60),
+          "Under":under['P'],
+          "Odd":under['C']
+            }
+            db.append(dict)
+
+             
+      except Exception as e  :
+        pass
+  except Exception as e:
+  
+     pass
+
+# updating sheet
+ df = pd.DataFrame()
+ dbold.extend(db)
+ df = df.append(dbold, ignore_index=True) 
+ dfi  = df.drop_duplicates(subset=["Home", "Away"], keep='first')
+ sheet.update([dfi.columns.values.tolist()] +dfi.values.tolist())
+
+schedule.every(15).seconds.do(signals)
 
 if __name__ == '__main__':
     while True:
